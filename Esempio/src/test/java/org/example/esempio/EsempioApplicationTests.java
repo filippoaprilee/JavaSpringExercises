88465e13227007e629c4dfda18d213b504e3b41f
filippoaprilee <filippoaprilee@gmail.com>
2024-06-21 12:35:46 +0200

    Update Esempio

---
+++ Esempio/src/test/java/org/example/esempio/EsempioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.esempio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
