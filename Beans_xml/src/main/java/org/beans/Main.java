0e043fb50a98e4e86b9613e77c2f70faa6ef6ed8
filippoaprilee <filippoaprilee@gmail.com>
2024-06-24 12:32:40 +0200

    Update

--- Beans_xml/src/main/java/org/beans/Main.java
+++ Beans_xml/src/main/java/org/beans/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Ordine ordine = context.getBean("ordine", Ordine.class);
+//        Ordine ordine = context.getBean("ordine", Ordine.class);
+//
+//        System.out.println(ordine.HelloWorld());
 
-        System.out.println(ordine.HelloWorld());
+        System.out.println(context.getBean("ordine", Ordine.class).HelloWorld());
     }
 }
