1211c1887e39363b0cd34d06db96038d35a3b313
filippoaprilee <filippoaprilee@gmail.com>
2024-06-25 12:26:04 +0200

    Update

--- AnnotationEsempio/src/main/java/org/annotationesempio/BeanA.java
+++ AnnotationEsempio/src/main/java/org/annotationesempio/BeanA.java
@@ -1,10 +1,15 @@
 package org.annotationesempio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeanA {
     String message="ciao sono io";
+
+    @Value("string value")
+    private String stringValue;
+
     public String hello() {
         return message;
     }
