5bdb2f1b909bd39bf39d406f2426cee1eb755fb5
filippoaprilee <filippoaprilee@gmail.com>
2024-06-25 12:49:40 +0200

    Update

---
+++ AnnotationAutowired/src/main/java/org/annotationeautowired/ClienteService.java
@@ -0,0 +1,7 @@
+package org.annotationeautowired;
+
+public class ClienteService {
+    private String nome;
+    private String cognome;
+
+}
---
+++ AnnotationAutowired/src/main/java/org/annotationeautowired/OrdineService.java
@@ -0,0 +1,23 @@
+package org.annotationeautowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component(value="ordine")
+public class OrdineService {
+    private ClienteService cliente;
+
+    @Autowired
+    public OrdineService(ClienteService cliente) {
+        super();
+        this.cliente = cliente;
+    }
+
+    public OrdineService() {
+        super();
+    }
+
+    public String sayHello() {
+        return "ciao sono un odine del cliente " + cliente.getCliente(123) + " e dico " + cliente.sayHello();
+    }
+}
