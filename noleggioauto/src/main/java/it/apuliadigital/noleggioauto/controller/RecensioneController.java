26f888ac325371f90cd032add193a409632d5427
filippoaprilee <filippoaprilee@gmail.com>
2024-06-27 15:05:36 +0200

    Nuovo progetto (noleggio auto)

--- noleggioauto/src/main/java/it/apuliadigital/noleggioauto/controller/RecensioneController.java
+++ noleggioauto/src/main/java/it/apuliadigital/noleggioauto/controller/RecensioneController.java
@@ -2,9 +2,9 @@ package it.apuliadigital.noleggioauto.controller;
 
 import it.apuliadigital.noleggioauto.service.RecensioneService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import it.apuliadigital.noleggioauto.model.Recensione;
+import java.util.List;
 
 @RestController
 @RequestMapping("/recensioni")
@@ -13,7 +13,30 @@ public class RecensioneController {
     @Autowired
     private RecensioneService recensioneService;
 
-    @GetMapping
-    public List<>
+    @GetMapping("/get")
+    public List<Recensione> trovaTutteLeRecensioni() {
+        return recensioneService.trovaTutteLeRecensioni();
+    }
+
+    @GetMapping("/get/{id}")
+    public Recensione trovaRecensionePerId(@PathVariable Long id) {
+        return recensioneService.trovaRecensionePerId(id);
+    }
+
+    @PostMapping("/add")
+    public void aggiungiRecensione(@RequestBody Recensione recensione) {
+        recensioneService.aggiungiRecensione(recensione);
+    }
+
+    @PutMapping("/update/{id}")
+    public void modificaRecensione(@PathVariable Long id, @RequestBody Recensione recensione) {
+        recensione.setId(id);
+        recensioneService.modificaRecensione(recensione);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void eliminaRecensione(@PathVariable Long id) {
+        recensioneService.eliminaRecensione(id);
+    }
 
 }
