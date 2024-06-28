c4a50b7232b341a60cc31f93d44ceebaea65d587
filippoaprilee <filippoaprilee@gmail.com>
2024-06-28 14:12:19 +0200

    Update

--- apirestful/apirestful/src/main/java/it/apuliadigital/prova_api/controller/PersonaController.java
+++ apirestful/apirestful/src/main/java/it/apuliadigital/prova_api/controller/PersonaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -15,6 +16,11 @@ public class PersonaController {
 
     @Autowired
     private PersonaService personaService;
+
+    @GetMapping("/")
+    public RedirectView redirectView() {
+        return new RedirectView("index.html");
+    }
 
     @GetMapping("/get")
     public ResponseEntity<?> getAllPersone() {
