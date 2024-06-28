2a147697e60c09399e453227b616879b8315da47
filippoaprilee <filippoaprilee@gmail.com>
2024-06-28 21:31:51 +0200

    Update

---
+++ eserciziohtml/src/main/java/it/apuliadigital/eserciziohtml/EserciziohtmlApplication.java
@@ -0,0 +1,13 @@
+package it.apuliadigital.eserciziohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziohtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EserciziohtmlApplication.class, args);
+    }
+
+}
